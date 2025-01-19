46414887113bf6662b1a408ab63b7d3093ed5b3d
djliterat1984 <50715680+djliterat1984@users.noreply.github.com>
2025-01-19 19:53:20 +0200

    push files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const userRouter = require("./routes/userRouter.js");
+
+const app = express();
+
+const { PORT } = process.env;
+
+app.listen(PORT || 5001, () => {
+  console.log(`run on ${PORT || 5001}`);
+});
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173","https://auth-app-28w4.onrender.com"],
+  })
+);
+
+app.use("/api/user", userRouter);
